e77b04894a05605d3f9421852afec4fe94117a42
Pyrus-vagus <grushadaria@gmail.com>
2021-08-12 20:33:28 +0300

    create a tabview with 2 tabs, enable add/delete/update functionality

--- sources/views/data.js
+++ sources/views/data.js
@@ -1,11 +1,68 @@
 import {JetView} from "webix-jet";
-import {data} from "models/records";
+import {countries} from "models/countries";
+import {statuses} from "models/statuses";
 
 export default class DataView extends JetView{
 	config(){
-		return { view:"datatable", autoConfig:true, css:"webix_shadow_medium" };
+		const countriesCell = {
+			header:"Countries",
+			body:{
+				rows:[
+					{view: "button", value: "Add new", click:function(){
+						countries.add({
+							Name: "New Country"
+						  }, 0);
+					} },  
+			  {view:"datatable",
+			  editable:true,
+			  editaction: "dblclick",
+			  columns:[
+				{id: "Name", header: "Country", fillspace: true, editor: "text"  },
+				{template: "{common.trashIcon()}"},
+				],
+			  id: "countries",
+			  onClick:{
+				"wxi-trash": function(e,id){
+				  countries.remove(id);
+				  return false;
+				}
+			  }}]  
+		   }}
+		  const statusesCell = {
+			header:"Statuses", 
+			body:{
+			  rows:[
+			  {view: "button", value: "Add new", click:function(){
+				statuses.add({
+					Name: "New Status"
+				  }, 0);
+			}},  	 
+			  {view:"datatable",
+			  editable:true,
+			  editaction: "dblclick",
+			  columns:[
+				 {id: "Name", header: "Status",  fillspace: true, editor: "text" },
+				 {id: "Icon", header: "", template:"<span class='webix_icon fas fa-#Icon# '></span>", editor: "richselect", collection:["cogs", "user"]},
+				{template: "{common.trashIcon()}"},
+				],
+			  id: "statuses",
+			  onClick:{
+				"wxi-trash": function(e,id){
+				  statuses.remove(id);
+				  return false;
+				}
+			  }}]  
+			}
+		  } 
+	  const multiview = { 
+		type: "clean",
+		view:"tabview",
+		cells:[ countriesCell,  statusesCell]     
+	   }
+		return multiview;
 	}
-	init(view){
-		view.parse(data);
+	init(){
+		this.$$("countries").parse(countries);
+		this.$$("statuses").parse(statuses);
 	}
 }
