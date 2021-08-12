4796ecc79ae07f6847d6317853072e82e94823ab
Pyrus-vagus <grushadaria@gmail.com>
2021-08-12 20:39:16 +0300

    implement segmented button 'RU' and 'EN'

--- sources/views/settings.js
+++ sources/views/settings.js
@@ -1,4 +1,16 @@
-export default {
-  template: "Settings page",
-  css: "webix_shadow_medium app_start",
-};
+import { JetView } from "webix-jet";
+
+export default class SettingsView extends JetView {
+  config() {
+    const segmented = {
+      view: "segmented",
+      value:"En", inputWidth:250, options:[
+        { id:"en", value:"En" },
+        { id:"ru", value:"Ru"},
+        
+      ]
+    };
+    return segmented;
+  }
+ 
+}
