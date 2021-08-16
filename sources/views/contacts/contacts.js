af64bc589d7827bdeb8bb34f25806c3fe25c0446
pyrus-vagus <grushadaria@gmail.com>
2021-08-16 18:18:00 +0300

    connect list and form

--- sources/views/contacts/contacts.js
+++ sources/views/contacts/contacts.js
@@ -6,20 +6,35 @@ export default class ContactsView extends JetView {
   config() {
     const list = {
       view: "list",
-      id: "list",
+      localId: "list",
       select: true,
       template: function (obj) {
         return `${obj.Name}, ${obj.Email}, from ${
           countries.getItem(obj.Country).Name
         }`;
       },
+      on: {
+        onAfterSelect: (id) => {
+          this.setParam("id", id, true);
+        },
+      },
     };
     return {
-      cols: [list, FormView],
+      cols: [list, { $subview: "contacts.form" }],
       css: "webix_shadow_medium",
     };
   }
   init() {
     this.$$("list").parse(contacts);
+    this.$$("list").select(this.$$("list").getFirstId());
+  }
+  urlChange() {
+    contacts.waitData.then(() => {
+      const id = this.getParam("id") || contacts.getFirstId();
+      if (contacts.exist(id)) {
+        this.$$("list").select(id);
+        this.$$("list").showItem(id);
+      }
+    });
   }
 }
--- sources/views/contacts/form.js
+++ sources/views/contacts/form.js
@@ -1,26 +1,34 @@
 import { JetView } from "webix-jet";
+import { contacts } from "../../models/contacts";
 
 import { countries } from "../../models/countries";
 import { statuses } from "../../models/statuses";
 
 export default class FormView extends JetView {
   config() {
-    const form = {
-      type: "clean",
+    return {
       rows: [
         {
           view: "form",
+          localId: "form",
           elements: [
             { type: "section", template: "Edit Contacts" },
-            { view: "text", name: "name", value: "", label: "Name" },
-            { view: "text", name: "email", value: "", label: "Email" },
+            { view: "text", name: "Name", value: "", label: "Name" },
+            { view: "text", name: "Email", value: "", label: "Email" },
+            { view: "text", name: "Country", value: "", label: "Country" },
+            { view: "text", name: "Status", value: "", label: "Status" },
             { view: "button", value: "Submit" },
           ],
         },
         {},
       ],
     };
-
-    return form;
+  }
+  urlChange() {
+    const id = this.getParam("id", true);
+    if (id && contacts.exists(id)) {
+      const values = contacts.getItem(id);
+      this.$$("form").setValues(values);
+    }
   }
 }
